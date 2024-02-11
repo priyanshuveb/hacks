ca7e567ac9f4ee02e316937f01c2e898165bad12
priyanshuveb <priyanshubindal@gmail.com>
2024-02-11 23:06:37 +0530

    cross func reentracy

---
+++ src/cross-function/Bank.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {ReentrancyGaurd} from "../utils/ReentrancyGaurd.sol";
+
+contract Bank is ReentrancyGaurd {
+    mapping(address => uint256) balanceOf;
+    uint256 public constant MIN_VALUE = 1 ether;
+
+    function deposit() external payable {
+        require(msg.value > MIN_VALUE, "Less than minimum deposit value");
+        balanceOf[msg.sender] += msg.value;
+    }
+
+    function withdraw() external nonReentrant {
+        require(balanceOf[msg.sender] > MIN_VALUE, "Less than minimum withdraw value");
+        uint256 balance = balanceOf[msg.sender];
+        (bool success,) = msg.sender.call{value: balance}("");
+        require(success, "transaction failed");
+        balanceOf[msg.sender] = 0;
+    }
+
+    function transfer() public {
+        
+    }
+
+    function totalBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function userBalance(address user) public view returns (uint256 bal) {
+        bal = balanceOf[user];
+    }
+}
